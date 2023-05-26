b4f8710ac52ccd7a24f128e8a40c56c075c76551
Andrii <maloy.club2@gmail.com>
2023-05-26 15:31:59 +0300

    main commit

---
+++ js/style.js
@@ -0,0 +1,30 @@
+function ToMainPage() {
+    // Переходимо на нову сторінку
+    window.location.href = 'main.html';
+}
+
+function ToAboutPage() {
+    // Переходимо на нову сторінку
+    window.location.href = 'about.html';
+}
+
+function ToNewsPage() {
+    // Переходимо на нову сторінку
+    window.location.href = 'news.html';
+}
+
+function ToCarouselPage() {
+    // Переходимо на нову сторінку
+    window.location.href = 'carousel.html';
+}
+
+function ToGalleryPage() {
+    // Переходимо на нову сторінку
+    window.location.href = 'gallery.html';
+ }
+
+function ToContactsPage() {
+    // Переходимо на нову сторінку
+    window.location.href = 'contacts.html';
+}
+
